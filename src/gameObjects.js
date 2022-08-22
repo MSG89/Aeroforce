d3a94b920c46d8a043e5182860bde4c38c35eaa5
MSG89 <111423362+MSG89@users.noreply.github.com>
2022-08-22 12:49:38 +0300

    added start screen puts up game screen

---
+++ src/gameObjects.js
@@ -0,0 +1,11 @@
+function initGameObjects() {
+    console.log('test');
+    startScreen = document.querySelector('.start-screen');
+    gameScreen = document.querySelector('.game-screen');
+
+    return{
+        startScreen,
+        gameScreen,
+    }
+
+}
